784600948a3930edefa94519c1e02a2ff9838385
Luis Rita <luismestrerita@gmail.com>
2016-07-04 10:16:39 +0100

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This will be the structure in which we sart building our App.
+      </div>
+    )
+  }
+}
+
+export default App;
